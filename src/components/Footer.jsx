d20f9e9497e2f6b60660975d694bdf7fa2337235
Hanoon <90821506+Hanoon02@users.noreply.github.com>
2022-11-16 19:33:10 +0530

    fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,6 +7,9 @@ export default function Footer() {
         <>
             <div className="bg-gray-400">
                 <div className="flex flex-col items-center justify-center">
+                    <div className="sm:hidden block bg-gradient-to-r to-[#DA6732] from-[#D93B47] py-2 mt-4 px-11 rounded-full hover:scale-105 transition-all hover:shadow-md">
+                        <p className=' text-backgroundPrimary font-medium'>Download App</p>
+                    </div>
                     <p className="sm:py-6 py-4 text-sm sm:text-lg">Toonsutra coming soon on Google and Apple</p>
                     <div className="flex">
                         <img src={require("../assets/android.png")} alt="android" className=" sm:h-12 h-9 px-2" />
